6353003b39e426efe773571eed54906c75b60feb
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-06-20 04:29:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let a=document.getElementById("line");
+let rotation=0;
+
+function rotate(){
+rotation +=1;
+
+line.style.transform=`rotate(${rotation}deg)`;
+
+requestAnimationFrame(rotate);
+}
+
+rotate();
